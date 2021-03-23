11120355219258e1576b86e5ff87d06d3a8737a0
Alisson Alcântara Amaral da Silva - ASV <alisson.silva@iteris.com.br>
2021-03-22 21:13:00 -0300

    Initial commit from Create Next App

---
+++ src/components/Main/stories.tsx
@@ -0,0 +1,16 @@
+import { Meta, Story } from '@storybook/react/types-6-0'
+import Main from '.'
+
+export default {
+  title: 'Main',
+  component: Main
+} as Meta
+
+export const Basic: Story = (args) => <Main {...args} />
+
+export const ComponentWithChangedValues: Story = (args) => <Main {...args} />
+
+ComponentWithChangedValues.args = {
+  title: 'React Diferenciado',
+  description: 'Discription'
+}
