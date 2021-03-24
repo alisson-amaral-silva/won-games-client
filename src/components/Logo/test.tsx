2566fb6c5325d77485dd8a3ec748b2b6b3065c7d
Alisson Alcântara Amaral da Silva - ASV <alisson.silva@iteris.com.br>
2021-03-23 22:19:08 -0300

    adicionando testes no componente logo e alterações de css's de acordo com a prop passada na logo

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,14 +1,50 @@
-import { render, screen } from '@testing-library/react'
-import Logo  from '.'
+import { render, screen } from "@testing-library/react";
+import { renderWithTheme } from "utils/tests/helper";
+import "jest-styled-components";
+import Logo from ".";
 
-describe('<Logo  />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Logo  />)
-    expect(
-      screen.getByRole('heading', { name: /Logo /i })
-    ).toBeInTheDocument()
+describe("<Logo  />", () => {
+  it("should render white label by default", () => {
+    renderWithTheme(<Logo />);
 
-    expect(container.firstChild).toMatchSnapshot()
-  })
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: "#FAFAFA",
+    });
+  });
 
-})
+  it("should render a black label when is passed", () => {
+    renderWithTheme(<Logo color="black" />);
+
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: "#030517",
+    });
+  });
+
+  it("should render a normal logo when size is default", () => {
+    renderWithTheme(<Logo />);
+
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: "11rem",
+    });
+  });
+
+  it("should render a bigger logo", () => {
+    renderWithTheme(<Logo size="large" />);
+
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: "20rem",
+    });
+  });
+
+  it("should render a bigger logo without text if hideOnMobile", () => {
+    renderWithTheme(<Logo hideOnMobile />);
+
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyleRule(
+      "width",
+      "5.8rem",
+      {
+        media: "(max-width: 768px)",
+      }
+    );
+  });
+});
