4179aa8d9d0f61c548cc08cea9d0ec5cdc411bec
Alisson Alcântara Amaral da Silva - ASV <alisson.silva@iteris.com.br>
2021-04-01 15:31:24 -0300

    criando componente Ribbon, sua estilização e seus testes

---
+++ src/components/Ribbon/test.tsx
@@ -0,0 +1,45 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helper'
+import Ribbon from '.'
+
+describe('<Ribbon  />', () => {
+  it('should render the text correctly', () => {
+    renderWithTheme(<Ribbon>Best Seller</Ribbon>)
+
+    expect(screen.getByText('Best Seller')).toBeInTheDocument()
+  })
+
+  it('should render with the primary color', () => {
+    renderWithTheme(<Ribbon>Best Seller</Ribbon>)
+
+    expect(screen.getByText('Best Seller')).toHaveStyle({
+      backgroundColor: '#f231a5'
+    })
+  })
+
+  it('should render with the secondary color', () => {
+    renderWithTheme(<Ribbon color="secondary">Best Seller</Ribbon>)
+
+    expect(screen.getByText('Best Seller')).toHaveStyle({
+      backgroundColor: '#3CD3C1'
+    })
+  })
+
+  it('should render with the normal size as default', () => {
+    renderWithTheme(<Ribbon>Best Seller</Ribbon>)
+
+    expect(screen.getByText('Best Seller')).toHaveStyle({
+      height: '3.6rem',
+      fontSize:'1.4rem'
+    })
+  })
+
+  it('should render with the small size as default', () => {
+    renderWithTheme(<Ribbon size='small'>Best Seller</Ribbon>)
+
+    expect(screen.getByText('Best Seller')).toHaveStyle({
+      height: '2.6rem',
+      fontSize:'1.2rem'
+    })
+  })
+})
