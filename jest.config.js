cc095d8cc63039f9fdea6bb58f0aa1025929d577
Alisson Alcântara Amaral da Silva - ASV <alisson.silva@iteris.com.br>
2021-04-09 11:26:20 -0300

    criação de testes do slider

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,5 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
-  modulePaths: ['<rootDir>/src/']
+  modulePaths: ['<rootDir>/src/', '<rootDir>/.jest']
 }
