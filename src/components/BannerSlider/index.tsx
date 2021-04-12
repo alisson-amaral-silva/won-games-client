45c2156c0e966913bb74070d9f6225cdb601f043
Alisson Alcântara Amaral da Silva - ASV <alisson.silva@iteris.com.br>
2021-04-12 19:39:39 -0300

    incluindo o banner slider vertical e horizontal dependendo da responsividade e testes

---
+++ src/components/BannerSlider/index.tsx
@@ -0,0 +1,37 @@
+import { BannerProps } from 'components/Banner'
+import Slider, {SliderSettings} from '../Slider'
+import Banner from '../Banner'
+import * as S from './styles'
+
+export type BannerSliderProps = {
+  items: BannerProps[]
+}
+
+const settings: SliderSettings = {
+  dots: true,
+  arrows: false,
+  vertical: true,
+  verticalSwiping: true,
+  infinite: false,
+  responsive:[
+    {
+      breakpoint: 1170,
+      settings:{
+        vertical:false,
+        verticalSwiping:false
+      }
+    }
+  ]
+}
+
+const BannerSlider = ({ items }: BannerSliderProps) => (
+  <S.Wrapper>
+    <Slider settings={settings}>
+      {items.map((item, index) => (
+        <Banner key={index} {...item} />
+      ))}
+    </Slider>
+  </S.Wrapper>
+)
+
+export default BannerSlider
