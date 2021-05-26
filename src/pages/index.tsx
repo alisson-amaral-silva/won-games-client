efef0ed3e6f646b4cdcbe93fb73b094b7ca1e394
Alisson Alcântara Amaral da Silva - ASV <alisson.silva@iteris.com.br>
2021-05-26 17:48:58 -0300

    criando mock da home e estrutura de componentes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,3 +2,18 @@ import Home from 'templates/Home'
 export default function Index() {
   return <Home />
 }
+
+  // Static => gerar estático em build time
+export function getStaticProps(){
+  // faz lógica
+  // pode ser buscar dados de API
+  // fazer calculo| leitura de context
+
+
+  // retorno dos dados
+
+}
+
+//ATENÇÃO:
+// os métodos getStaticProps/getServerSideProps SÓ FUNCIONA EM PAGES
+// CASO TENTE CHAMAR DENTRO DE COMPONENTS OU TEMPLATES, NÃO ROLA
--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -2,29 +2,71 @@ import Menu from 'components/Menu'
 import { Container } from 'components/Container'
 import Footer from 'components/Footer'
 import Heading from 'components/Heading'
+import { BannerProps } from 'components/Banner'
+import BannerSlider from 'components/BannerSlider'
+import GameCardSlider from 'components/GameCardSlider'
+import { GameCardProps } from 'components/GameCard'
+import Highlight, { HighlightProps } from 'components/Highlight'
 
-const Home = () => (
+export type HomeTemplateProps = {
+  banners: BannerProps[]
+  newGames: GameCardProps[]
+  mostPopularHighlight: HighlightProps
+  mostPopularGames: GameCardProps[]
+  upcomingGames: GameCardProps[]
+  upcomingHighlight: HighlightProps
+  upcomingMoreGames: GameCardProps[]
+  freeGames: GameCardProps[]
+  freeHighlight: HighlightProps
+}
+const Home = ({
+  banners,
+  newGames,
+  mostPopularGames,
+  mostPopularHighlight,
+  upcomingGames,
+  upcomingHighlight,
+  upcomingMoreGames,
+  freeGames,
+  freeHighlight
+}: HomeTemplateProps) => (
   <section>
     <Container>
       <Menu />
+      <BannerSlider items={banners} />
     </Container>
 
     <Container>
       <Heading lineLeft lineColor="secondary" color="black">
         News
       </Heading>
+      <GameCardSlider items={newGames} color="black" />
     </Container>
 
     <Container>
-      <Heading lineLeft lineColor="secondary">Most Popular</Heading>
+      <Heading lineLeft lineColor="secondary">
+        Most Popular
+      </Heading>
+      <Highlight {...mostPopularHighlight} />
+      <GameCardSlider items={newGames} />
     </Container>
 
     <Container>
-      <Heading lineLeft lineColor="secondary">Upcoming</Heading>
+      <Heading lineLeft lineColor="secondary">
+        Upcoming
+      </Heading>
+
+      <GameCardSlider items={newGames} />
+      <Highlight {...mostPopularHighlight} />
+      <GameCardSlider items={newGames} />
     </Container>
 
     <Container>
-      <Heading lineLeft lineColor="secondary">Free Games</Heading>
+      <Heading lineLeft lineColor="secondary">
+        Free Games
+      </Heading>
+      <Highlight {...mostPopularHighlight} />
+      <GameCardSlider items={newGames} />
     </Container>
 
     <Container>
