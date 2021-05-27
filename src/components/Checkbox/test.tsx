3b21744ba376826e4a54c99d44611da1f7778d50
Alisson Alcântara Amaral da Silva - ASV <alisson.silva@iteris.com.br>
2021-05-27 11:56:31 -0300

    criando opção de labelColor

--- src/components/Checkbox/test.tsx
+++ src/components/Checkbox/test.tsx
@@ -9,7 +9,18 @@ describe('<Checkbox  />', () => {
     expect(screen.getByRole('checkbox')).toBeInTheDocument()
     expect(screen.getByLabelText(/checkbox label/i)).toBeInTheDocument()
     expect(screen.getByText(/checkbox label/i)).toHaveAttribute('for','check')
-
   })
 
+  it('should render without label', () => {
+    renderWithTheme(<Checkbox />)
+
+    //sempre que precisa de buscar algo que pode não ter na tela, usar queryByWhatever
+    expect(screen.queryByLabelText('Checkbox')).not.toBeInTheDocument()
+  })
+
+  it('should render without black label', () => {
+    renderWithTheme(<Checkbox label="checkbox label" labelFor="check" labelColor="black" />)
+
+    expect(screen.getByText(/checkbox label/i)).toHaveStyle({color:'#030517'})
+  })
 })
