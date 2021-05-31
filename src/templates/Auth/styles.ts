1f2f7c6c0b5591f01a65077398c2a7f814fb9e01
Alisson Alcântara Amaral da Silva - ASV <alisson.silva@iteris.com.br>
2021-05-31 18:36:51 -0300

    criando estilos do template desktop

--- src/templates/Auth/styles.ts
+++ src/templates/Auth/styles.ts
@@ -1,11 +1,88 @@
-import styled from 'styled-components'
+import * as HeadingStyles from 'components/Heading/styles'
+import * as LogoStyles from 'components/Logo/styles'
+import styled, { css } from 'styled-components'
 
-export const Wrapper = styled.main``
+export const Wrapper = styled.main`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  height: 100vh;
+`
 
-export const BannerBlock = styled.div``
+export const BannerBlock = styled.div`
+  ${({ theme }) => css`
+    position: relative;
+    background-image: url(/img/auth-bg.jpg);
+    background-size: cover; //preencher todo o espaço
+    background-position: center center; // property sets the starting position of a background image.
 
-export const Subtitle = styled.h3``
+    padding: ${theme.spacings.xxlarge} ${theme.spacings.xxlarge}
+      ${theme.spacings.large};
 
-export const Footer = styled.p``
+    &:after {
+      content: '';
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      background: ${theme.colors.black};
+      opacity: 0.85;
+    }
+  `}
+`
 
-export const Content = styled.div``
+export const BannerContent = styled.div`
+  ${({ theme }) => css`
+    //trazer conteudo do banner à esquerda à frente da imagem
+    position: relative;
+    z-index: ${theme.layers.base};
+    color: ${theme.colors.white};
+    display: grid;
+    grid-template-columns: 1fr;
+    justify-content: space-between;
+    height: 100%;
+  `}
+`
+
+export const Subtitle = styled.h3`
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.xxlarge};
+    font-weight: ${theme.font.light};
+    margin-top: ${theme.spacings.xxsmall};
+
+    strong {
+      color: ${theme.colors.primary};
+    }
+  `}
+`
+
+export const Footer = styled.p`
+  ${({ theme }) => css`
+    text-align: center;
+    font-size: ${theme.font.sizes.xsmall};
+    align-self: end; //alinha o final do espaço que ele tem
+  `}
+`
+
+export const Content = styled.div`
+  ${({ theme }) => css`
+    background: ${theme.colors.white};
+    display: grid;
+    align-items: center;
+    justify-content: center;
+  `}
+`
+
+export const ContentWrapper = styled.div`
+  ${({ theme }) => css`
+    width: 30rem;
+
+    ${LogoStyles.Wrapper} {
+      margin: 0 auto ${theme.spacings.xxlarge};
+    }
+
+    ${HeadingStyles.Wrapper} {
+      margin-bottom: ${theme.spacings.medium};
+    }
+  `}
+`
