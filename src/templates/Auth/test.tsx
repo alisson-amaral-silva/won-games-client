8581e881d780b1a2f55c1e797929fb8ea9336ff5
Alisson Alcântara Amaral da Silva - ASV <alisson.silva@iteris.com.br>
2021-05-31 17:48:44 -0300

    incluindo testes para o template

--- src/templates/Auth/test.tsx
+++ src/templates/Auth/test.tsx
@@ -1,5 +1,6 @@
 import { renderWithTheme } from 'utils/tests/helper'
 import Auth from '.'
+import { screen } from '@testing-library/react'
 
 describe('<Auth  />', () => {
   it('should render all components children', () => {
@@ -8,8 +9,29 @@ describe('<Auth  />', () => {
         <input type="text" />{' '}
       </Auth>
     )
-    // expect(
-    //   screen.getByRole('heading', { name: /Auth /i })
-    // ).toBeInTheDocument()
+    // verifiquem se tem 2 logos
+    expect(screen.getAllByRole('img', { name: /won games/i })).toHaveLength(2)
+
+    // verifica se tem o heading principal do banner
+    expect(
+      screen.getByRole('heading', {
+        name: /All your favorite games in one place/i
+      })
+    ).toBeInTheDocument()
+
+    // verifica se tem o subtitle
+    expect(
+      screen.getByRole('heading', {
+        name: /WON is the best and most complete game platform/i
+      })
+    ).toBeInTheDocument()
+
+    // verifica se tem o title do content
+    expect(
+      screen.getByRole('heading', { name: /auth title/i })
+    ).toBeInTheDocument()
+
+    // verifica se o children tá sendo renderizado
+    expect(screen.getByRole('textbox')).toBeInTheDocument()
   })
 })
