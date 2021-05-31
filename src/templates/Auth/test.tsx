a08915da3c2c26b1048bb90e25df5aa9912ad801
Alisson Alcântara Amaral da Silva - ASV <alisson.silva@iteris.com.br>
2021-05-31 14:41:47 -0300

    criando estrutura inicial do template

--- src/templates/Auth/test.tsx
+++ src/templates/Auth/test.tsx
@@ -1,8 +1,13 @@
+import { renderWithTheme } from 'utils/tests/helper'
 import Auth from '.'
 
 describe('<Auth  />', () => {
-  it('should render the heading', () => {
-    // const { container } = render(<Auth  />)
+  it('should render all components children', () => {
+    renderWithTheme(
+      <Auth title="Auth title">
+        <input type="text" />{' '}
+      </Auth>
+    )
     // expect(
     //   screen.getByRole('heading', { name: /Auth /i })
     // ).toBeInTheDocument()
