1a04075c30fc69d5d526b10d31ae908f282e5634
Alisson Alcântara Amaral da Silva - ASV <alisson.silva@iteris.com.br>
2021-06-21 18:58:13 -0300

    alterando a estilização do componente gameInfo e seu respectivo story

--- src/components/GameInfo/stories.tsx
+++ src/components/GameInfo/stories.tsx
@@ -1,9 +1,20 @@
 import { Meta, Story } from '@storybook/react/types-6-0'
 import GameInfo, { GameInfoProps } from '.'
+import mockGame from './mock'
 
 export default {
   title: 'GameInfo',
-  component: GameInfo
+  component: GameInfo,
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  },
+  args: mockGame
 } as Meta
 
-export const Basic: Story<GameInfoProps> = (args) => <GameInfo {...args} />
+export const Basic: Story<GameInfoProps> = (args) => (
+  <div style={{ maxWidth: '144rem', padding: '1.5rem' }}>
+    <GameInfo {...args} />
+  </div>
+)
