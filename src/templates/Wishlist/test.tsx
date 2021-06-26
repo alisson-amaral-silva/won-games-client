de154ea216b966b6b4703535c717e5b3418cc32a
Alisson Amaral <aalissonamaral98@gmail.com>
2021-06-26 10:15:59 -0300

    incluindo showcase e menu nos testes

--- src/templates/Wishlist/test.tsx
+++ src/templates/Wishlist/test.tsx
@@ -1,12 +1,32 @@
 import { renderWithTheme } from 'utils/tests/helper'
 import { screen } from '@testing-library/react'
-import Wishlist from '.'
+import Wishlist, { WishlistTemplateProps } from '.'
+import gamesMock from 'components/GameCardSlider/mock'
+import highlightMock from 'components/Highlight/mock'
+
+const props: WishlistTemplateProps = {
+  recommendedHighlight: highlightMock,
+  recommendedGames: gamesMock
+}
+
+jest.mock('components/Menu', () => ({
+  __esModule: true,
+  default: function Mock() {
+    return <div data-testid="Mock Menu" />
+  }
+}))
+
+jest.mock('components/Showcase', () => ({
+  __esModule: true,
+  default: function Mock() {
+    return <div data-testid="Mock Showcase" />
+  }
+}))
 
 describe('<Wishlist  />', () => {
   it('should render the heading', () => {
-    renderWithTheme(<Wishlist />)
-    expect(
-      screen.getByRole('heading', { name: /Wishlist/i })
-    ).toBeInTheDocument()
+    renderWithTheme(<Wishlist {...props} />)
+    expect(screen.getByTestId('Mock Showcase')).toBeInTheDocument()
+    expect(screen.getByTestId('Mock Menu')).toBeInTheDocument()
   })
 })
