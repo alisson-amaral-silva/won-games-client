c6b98ac4be4da0aac1029fb9f2dee62e5c1c1d18
Alisson Amaral <aalissonamaral98@gmail.com>
2021-07-07 19:25:55 -0300

    logica de abrir/fechar o drop down

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import * as S from './styles'
 
 export type DropdownProps = {
@@ -6,11 +7,12 @@ export type DropdownProps = {
 }
 
 const Dropdown = ({ title, children }: DropdownProps) => {
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
-    <S.Wrapper>
-      <h1>{title}</h1>
-      <S.Title>{title}</S.Title>
-      <S.Content>{children}</S.Content>
+    <S.Wrapper isOpen={isOpen}>
+      <S.Title onClick={() => setIsOpen(!isOpen)}>{title}</S.Title>
+      <S.Content aria-hidden={!isOpen}>{children}</S.Content>
     </S.Wrapper>
   )
 }
--- src/components/Dropdown/test.tsx
+++ src/components/Dropdown/test.tsx
@@ -1,12 +1,21 @@
 import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { renderWithTheme } from 'utils/tests/helper'
 import Dropdown from '.'
 
 describe('<Dropdown  />', () => {
-  it('should render the heading', () => {
-    renderWithTheme(<Dropdown title="something">Something</Dropdown>)
-    expect(
-      screen.getByRole('heading', { name: /something/i })
-    ).toBeInTheDocument()
+  it('should render the closed dropdown', () => {
+    renderWithTheme(<Dropdown title="something">Shazam</Dropdown>)
+    expect(screen.getByText(/something/i)).toBeInTheDocument()
+    expect(screen.queryByText(/shazam/i)).toHaveAttribute('aria-hidden', 'true')
+  })
+
+  it('should open dropdown', () => {
+    renderWithTheme(<Dropdown title="something">Shazam</Dropdown>)
+    userEvent.click(screen.getByText(/something/i))
+    expect(screen.queryByText(/shazam/i)).toHaveAttribute(
+      'aria-hidden',
+      'false'
+    )
   })
 })
