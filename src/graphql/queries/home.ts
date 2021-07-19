d1cef82455405bc88aa82623d0f14dc5e7e99b56
Alisson Alcântara Amaral da Silva - ASV <alisson.silva@iteris.com.br>
2021-07-19 17:42:38 -0300

    ajustando queries da home para free/uppcoming/new

--- src/graphql/queries/home.ts
+++ src/graphql/queries/home.ts
@@ -1,12 +1,33 @@
 import { gql } from '@apollo/client'
 import { BannerFragment } from 'graphql/fragments/banner'
+import { GameFragment } from 'graphql/fragments/game'
 
 export const GET_HOME = gql`
   query GetHome {
     banners {
       ...BannerFragment
     }
+    newGames: games(
+      where: { release_date_lte: "2021-07-19" }
+      sort: "release_date:desc"
+      limit: 8
+    ) {
+      ...GameFragment
+    }
+
+    upcommingGames: games(
+      where: { release_date_gt: "2021-07-19" }
+      sort: "release_date:asc"
+      limit: 8
+    ) {
+      ...GameFragment
+    }
+
+    freeGames: games(where: { price: 0 }, sort: "release_date:desc", limit: 8) {
+      ...GameFragment
+    }
   }
 
+  ${GameFragment}
   ${BannerFragment}
 `
