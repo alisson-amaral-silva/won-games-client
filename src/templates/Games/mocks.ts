7cc4e87ae06445eef483da25e127ff7388ca4ac1
Alisson Amaral <aalissonamaral98@gmail.com>
2021-08-18 23:25:30 -0300

    Ajustando teste dos template de games

--- src/templates/Games/mocks.ts
+++ src/templates/Games/mocks.ts
@@ -3,7 +3,7 @@ import { GET_GAMES } from 'graphql/queries/games'
 export const gamesMock = {
   request: {
     query: GET_GAMES,
-    variables: { limit: 15 }
+    variables: { limit: 15, where: {} }
   },
   result: {
     data: {
@@ -26,7 +26,7 @@ export const gamesMock = {
 export const fetchMoreMock = {
   request: {
     query: GET_GAMES,
-    variables: { limit: 15, start: 1 }
+    variables: { limit: 15, start: 1, where: {} }
   },
   result: {
     data: {
