4a610a918be64e2bcce5d4a5565987265a31ffad
Alisson Amaral <aalissonamaral98@gmail.com>
2021-08-24 09:30:21 -0300

    criação do overlay do dropdown

--- src/components/Dropdown/test.tsx
+++ src/components/Dropdown/test.tsx
@@ -2,20 +2,39 @@ import { screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { renderWithTheme } from 'utils/tests/helper'
 import Dropdown from '.'
-
-describe('<Dropdown  />', () => {
-  it('should render the closed dropdown', () => {
-    renderWithTheme(<Dropdown title="something">Shazam</Dropdown>)
-    expect(screen.getByText(/something/i)).toBeInTheDocument()
-    expect(screen.queryByText(/shazam/i)).toHaveAttribute('aria-hidden', 'true')
+describe('<Dropdown />', () => {
+  beforeEach(() => {
+    const title = <h1 aria-label="toogle dropdown">Click here</h1>
+    renderWithTheme(
+      <Dropdown title={title}>
+        <span>content</span>
+      </Dropdown>
+    )
+  })
+  it('should render title', () => {
+    expect(screen.getByLabelText(/toogle dropdown/)).toBeInTheDocument()
+  })
+  it('should handle open/close dropdown', () => {
+    const content = screen.getByText(/content/).parentElement!
+    expect(content).toHaveStyle({ opacity: 0 })
+    expect(content.getAttribute('aria-hidden')).toBe('true')
+    userEvent.click(screen.getByLabelText(/toogle dropdown/))
+    expect(content).toHaveStyle({ opacity: 1 })
+    expect(content.getAttribute('aria-hidden')).toBe('false')
   })
 
-  it('should open dropdown', () => {
-    renderWithTheme(<Dropdown title="something">Shazam</Dropdown>)
-    userEvent.click(screen.getByText(/something/i))
-    expect(screen.queryByText(/shazam/i)).toHaveAttribute(
-      'aria-hidden',
-      'false'
-    )
+  it('should handle open/close dropdown when clicking on overlay', () => {
+    const content = screen.getByText(/content/).parentElement!
+    const overlay = content.nextElementSibling
+
+    userEvent.click(screen.getByLabelText(/toogle dropdown/))
+
+    expect(overlay).toHaveStyle({ opacity: 1 })
+    expect(overlay!.getAttribute('aria-hidden')).toBe('false')
+
+    userEvent.click(overlay!)
+
+    expect(overlay).toHaveStyle({ opacity: 0 })
+    expect(overlay!.getAttribute('aria-hidden')).toBe('true')
   })
 })
