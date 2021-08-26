815be7e1be051dca6b5e90fa28edb856e4c62ae2
Alisson Amaral <aalissonamaral98@gmail.com>
2021-08-26 10:12:27 -0300

    incluindo useContext no cart dropdown

--- src/components/CartDropdown/test.tsx
+++ src/components/CartDropdown/test.tsx
@@ -6,18 +6,16 @@ import CartDropdown from '.'
 import { CartContextDefaultValues } from 'hooks/use-cart'
 
 describe('<CartDropdown />', () => {
-  // it('should render <CartIcon /> and its badge', () => {
-  //   render(<CartDropdown items={items} total="R$ 300,00" />)
-  //   expect(screen.getByLabelText(/shopping cart/i)).toBeInTheDocument()
-  //   expect(screen.getByText(`${items.length}`)).toBeInTheDocument()
-  // })
-  it('should render Dropdown content with cart items and total', () => {
+  beforeEach(() => {
     const cartProviderProps = {
       ...CartContextDefaultValues,
       items,
       total: 'R$ 300,00'
     }
     render(<CartDropdown />, { cartProviderProps })
+  })
+
+  it('should render Dropdown content with cart items and total', () => {
     expect(screen.getByText('R$ 300,00')).toBeInTheDocument()
     expect(screen.getByText(`${items[0].title}`)).toBeInTheDocument()
   })
