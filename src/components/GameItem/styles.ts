78a35b9ba74b860cb205ffcef0f3d1463c9c8aea
Alisson Amaral <aalissonamaral98@gmail.com>
2021-08-26 22:20:10 -0300

    incluindo o texto remove dentro do gameItem

--- src/components/GameItem/styles.ts
+++ src/components/GameItem/styles.ts
@@ -14,6 +14,7 @@ export const Wrapper = styled.div`
 
 export const GameContent = styled.div`
   display: flex;
+  width: 100%;
 `
 
 export const ImageBox = styled.div`
@@ -38,6 +39,7 @@ export const Content = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
+  width: 100%;
 `
 
 export const Title = styled.h3`
@@ -69,6 +71,21 @@ export const DownloadLink = styled.a`
   ${({ theme }) => css`
     color: ${theme.colors.white};
     margin-left: ${theme.spacings.xxsmall};
+  `}
+`
+
+export const Group = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+`
+
+export const Remove = styled.div`
+  ${({ theme }) => css`
+    cursor: pointer;
+    color: ${theme.colors.primary};
+    font-size: ${theme.font.sizes.small};
   `}
 `
 
