f5b7d5a13f7ec54026c7a26a63ea185c2bc99c2c
Alisson Amaral <aalissonamaral98@gmail.com>
2021-09-02 13:28:30 -0300

    inclusão do username e do sign out

--- src/components/ProfileMenu/index.tsx
+++ src/components/ProfileMenu/index.tsx
@@ -6,42 +6,52 @@ import {
   ExitToApp,
   FormatListBulleted
 } from '@styled-icons/material-outlined'
+import { signOut } from 'next-auth/client'
 
 export type ProfileMenuProps = {
   activeLink?: '/profile/me' | '/profile/cards' | '/profile/orders' | string
 }
 
-const ProfileMenu = ({ activeLink }: ProfileMenuProps) => (
-  <S.Nav>
-    {/* passHref - Forces Link to send the href property to its child. Defaults to false */}
-    <Link href="/profile/me" passHref>
-      <S.Link isActive={activeLink === '/profile/me'} title="My profile">
-        <AccountCircle size={24} />
-        <span>My profile</span>
-      </S.Link>
-    </Link>
+const ProfileMenu = ({ activeLink }: ProfileMenuProps) => {
+  const handleSignOut = () => {
+    signOut()
+  }
 
-    <Link href="/profile/cards" passHref>
-      <S.Link isActive={activeLink === '/profile/cards'} title="My cards">
-        <CreditCard size={24} />
-        <span>My cards</span>
-      </S.Link>
-    </Link>
+  return (
+    <S.Nav>
+      {/* passHref - Forces Link to send the href property to its child. Defaults to false */}
+      <Link href="/profile/me" passHref>
+        <S.Link isActive={activeLink === '/profile/me'} title="My profile">
+          <AccountCircle size={24} />
+          <span>My profile</span>
+        </S.Link>
+      </Link>
 
-    <Link href="/profile/orders" passHref>
-      <S.Link isActive={activeLink === '/profile/orders'} title="My orders">
-        <FormatListBulleted size={24} />
-        <span>My orders</span>
-      </S.Link>
-    </Link>
+      <Link href="/profile/cards" passHref>
+        <S.Link isActive={activeLink === '/profile/cards'} title="My cards">
+          <CreditCard size={24} />
+          <span>My cards</span>
+        </S.Link>
+      </Link>
 
-    <Link href="/logout" passHref>
-      <S.Link title="Sign out">
-        <ExitToApp size={24} />
+      <Link href="/profile/orders" passHref>
+        <S.Link isActive={activeLink === '/profile/orders'} title="My orders">
+          <FormatListBulleted size={24} />
+          <span>My orders</span>
+        </S.Link>
+      </Link>
+
+      <S.Link
+        data-testid="sign-out"
+        role="button"
+        onClick={handleSignOut}
+        title="Sign out"
+      >
+        <ExitToApp />
         <span>Sign out</span>
       </S.Link>
-    </Link>
-  </S.Nav>
-)
+    </S.Nav>
+  )
+}
 
 export default ProfileMenu
--- src/components/UserDropdown/index.tsx
+++ src/components/UserDropdown/index.tsx
@@ -7,44 +7,49 @@ import {
   ExitToApp
 } from '@styled-icons/material-outlined'
 import { ChevronDown } from '@styled-icons/boxicons-regular/ChevronDown'
+import { signOut } from 'next-auth/client'
 
 export type UserDropdownProps = {
   username: string
 }
 
-const UserDropdown = ({ username }: UserDropdownProps) => (
-  <Dropdown
-    title={
-      <>
-        <AccountCircle size={24} />
-        <S.Username>{username}</S.Username>
-        <ChevronDown size={24} />
-      </>
-    }
-  >
-    <S.Nav>
-      <Link href="/profile/me" passHref>
-        <S.Link>
-          <AccountCircle />
-          <span>My profile</span>
-        </S.Link>
-      </Link>
+const UserDropdown = ({ username }: UserDropdownProps) => {
+  const handleSignOut = () => {
+    signOut()
+  }
 
-      <Link href="/wishlist" passHref>
-        <S.Link>
-          <FavoriteBorder />
-          <span>Wishlist</span>
-        </S.Link>
-      </Link>
+  return (
+    <Dropdown
+      title={
+        <>
+          <AccountCircle size={24} />
+          <S.Username>{username}</S.Username>
+          <ChevronDown size={24} />
+        </>
+      }
+    >
+      <S.Nav>
+        <Link href="/profile/me" passHref>
+          <S.Link>
+            <AccountCircle />
+            <span>My profile</span>
+          </S.Link>
+        </Link>
 
-      <Link href="/logout" passHref>
-        <S.Link>
+        <Link href="/wishlist" passHref>
+          <S.Link>
+            <FavoriteBorder />
+            <span>Wishlist</span>
+          </S.Link>
+        </Link>
+
+        <S.Link role="button" onClick={handleSignOut} title="Sign out">
           <ExitToApp />
           <span>Sign out</span>
         </S.Link>
-      </Link>
-    </S.Nav>
-  </Dropdown>
-)
+      </S.Nav>
+    </Dropdown>
+  )
+}
 
 export default UserDropdown
