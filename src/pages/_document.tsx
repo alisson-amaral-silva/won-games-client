991f8541639d365edd30e90dc74f258bb1bcaa4f
Alisson Amaral <aalissonamaral98@gmail.com>
2021-09-04 23:40:52 -0300

    ajuste de identação

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,11 @@
 import Document, {
-  DocumentContext,
   Html,
   Head,
   Main,
-  NextScript
+  NextScript,
+  DocumentContext
 } from 'next/document'
+
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
@@ -36,7 +37,7 @@ export default class MyDocument extends Document {
 
   render() {
     return (
-      <Html lang="pt-BR">
+      <Html lang="en-US">
         <Head />
         <body>
           <Main />
