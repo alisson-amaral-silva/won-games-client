b9f839e351b0c19a83d1492ff9efd68d1251404c
Alisson Amaral <aalissonamaral98@gmail.com>
2021-09-05 14:00:17 -0300

    estrutura inicial do wishlist provider

---
+++ src/hooks/use-wishlist/index.tsx
@@ -0,0 +1,45 @@
+import { GameCardProps } from 'components/GameCard'
+import { createContext, useContext, useEffect, useState } from 'react'
+import Wishlist from 'templates/Wishlist'
+
+export type WishlistContextData = {
+  items: GameCardProps[]
+  isInWishlist: (id: string) => boolean
+  addToWishlist: (id: string) => void
+  removeFromWishlist: (id: string) => void
+  loading: boolean
+}
+
+export const WishlistContextDefaultValues = {
+  items: [],
+  isInWishlist: () => false,
+  addToWishlist: () => null,
+  removeFromWishlist: () => null,
+  loading: false
+}
+
+export const WishlistContext = createContext<WishlistContextData>(
+  WishlistContextDefaultValues
+)
+
+export type WishlistProviderProps = {
+  children: React.ReactNode
+}
+
+const WishlistProvider = ({ children }: WishlistProviderProps) => {
+  const isInWishlist = (id: string) => false
+  const addToWishlist = (id: string) => {}
+  const removeFromWishlist = (id: string) => {}
+
+  return (
+    <WishlistContext.Provider
+      value={{ addToWishlist, isInWishlist, removeFromWishlist }}
+    >
+      {children}
+    </WishlistContext.Provider>
+  )
+}
+
+const useWishlist = () => useContext(WishlistContext)
+
+export { WishlistProvider, useWishlist }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,7 @@ import { ThemeProvider } from 'styled-components'
 import GlobalStyles from 'styles/global'
 import theme from 'styles/theme'
 import { useApollo } from 'utils/apollo'
+import { WishlistProvider } from 'hooks/use-wishlist'
 function App({ Component, pageProps }: AppProps) {
   const client = useApollo(pageProps.initialApolloState)
 
@@ -16,24 +17,26 @@ function App({ Component, pageProps }: AppProps) {
       <ApolloProvider client={client}>
         <ThemeProvider theme={theme}>
           <CartProvider>
-            <Head>
-              <title>React Avançado - Boilerplate</title>
-              <link rel="shortcut icon" href="/img/icon-512.png" />
-              <link rel="apple-touch-icon" href="/img/icon-512.png" />
-              <link rel="manifest" href="/manifest.json" />
-              <meta
-                name="description"
-                content="A simple project starter to work with Typescript, react, nextJS and Styled Components"
+            <WishlistProvider>
+              <Head>
+                <title>React Avançado - Boilerplate</title>
+                <link rel="shortcut icon" href="/img/icon-512.png" />
+                <link rel="apple-touch-icon" href="/img/icon-512.png" />
+                <link rel="manifest" href="/manifest.json" />
+                <meta
+                  name="description"
+                  content="A simple project starter to work with Typescript, react, nextJS and Styled Components"
+                />
+              </Head>
+              <GlobalStyles />
+              <NextNprogress
+                color="#F231A5"
+                startPosition={0.3}
+                stopDelayMs={200}
+                height={5}
               />
-            </Head>
-            <GlobalStyles />
-            <NextNprogress
-              color="#F231A5"
-              startPosition={0.3}
-              stopDelayMs={200}
-              height={5}
-            />
-            <Component {...pageProps} />
+              <Component {...pageProps} />
+            </WishlistProvider>
           </CartProvider>
         </ThemeProvider>
       </ApolloProvider>
