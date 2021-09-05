50df5ee9cbc68f529c4e63f472c343ecb0a9af05
Alisson Amaral <aalissonamaral98@gmail.com>
2021-09-05 14:27:30 -0300

    estrutura inicial do wishlist e do passando informação pelo jwt

---
+++ src/hooks/use-wishlist/test.tsx
@@ -0,0 +1,17 @@
+import { MockedProvider } from '@apollo/client/testing'
+import { renderHook } from '@testing-library/react-hooks'
+import React from 'react'
+import { useWishlist, WishlistProvider } from '.'
+import { wishlistMock } from './mock'
+
+describe('useWishlist', () => {
+  it('should return wishlist items', () => {
+    const wrapper = ({ children }: { children: React.ReactNode }) => (
+      <MockedProvider mocks={[wishlistMock]}>
+        <WishlistProvider>{children}</WishlistProvider>
+      </MockedProvider>
+    )
+
+    const { result } = renderHook(() => useWishlist())
+  })
+})
