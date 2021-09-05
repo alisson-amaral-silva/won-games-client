30bb832974505cf9fec767ae917fe20f002a0ac2
Alisson Amaral <aalissonamaral98@gmail.com>
2021-09-05 18:34:54 -0300

    inclusão do wishlist provider para testes

--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -1,29 +1,41 @@
 import { render, RenderOptions } from '@testing-library/react'
+
 import {
   CartContext,
   CartContextData,
   CartContextDefaultValues
 } from 'hooks/use-cart'
+
+import {
+  WishlistContext,
+  WishlistContextData,
+  WishlistContextDefaultValues
+} from 'hooks/use-wishlist'
+
 import { ReactElement } from 'react'
 import { ThemeProvider } from 'styled-components'
 import theme from 'styles/theme'
 
 type CustomRenderPropos = {
   cartProviderProps?: CartContextData
+  wishlistProviderProps?: WishlistContextData
 } & Omit<RenderOptions, 'queries'>
 
 const customRender = (
   ui: ReactElement,
   {
     cartProviderProps = CartContextDefaultValues,
+    wishlistProviderProps = WishlistContextDefaultValues,
     ...renderOptions
   }: CustomRenderPropos = {}
 ) =>
   render(
     <ThemeProvider theme={theme}>
-      <CartContext.Provider value={cartProviderProps}>
-        {ui}
-      </CartContext.Provider>
+      <WishlistContext.Provider value={wishlistProviderProps}>
+        <CartContext.Provider value={cartProviderProps}>
+          {ui}
+        </CartContext.Provider>
+      </WishlistContext.Provider>
     </ThemeProvider>,
     renderOptions
   )
