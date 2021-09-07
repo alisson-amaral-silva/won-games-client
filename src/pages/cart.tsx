f71452828b30d8ab1f8b829ab845de3931f1f3f2
Alisson Amaral <aalissonamaral98@gmail.com>
2021-09-07 11:38:15 -0300

    protegendo a rota de cart contra usuarios não autenticados

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -2,17 +2,20 @@ import cartMock from 'components/CartList/mock'
 import cardsMock from 'components/PaymentOptions/mock'
 import { GetCart } from 'graphql/generated/GetCart'
 import { GET_CART } from 'graphql/queries/cart'
+import { GetServerSidePropsContext } from 'next'
 import Cart, { CartProps } from 'templates/Cart'
 import { initializeApollo } from 'utils/apollo'
 import { gamesMapper, highlightMapper } from 'utils/mappers'
+import protectedRoutes from 'utils/protected-routes'
 
 export default function CartPage(props: CartProps) {
   return <Cart {...props} />
 }
 
 // Cada vez que o usuario add ou remove as coisas ela não é estatica por isso o serverSideProps
-export async function getServerSideProps() {
-  const apolloClient = initializeApollo()
+export async function getServerSideProps(ctx: GetServerSidePropsContext) {
+  const session = await protectedRoutes(ctx)
+  const apolloClient = initializeApollo(null, session)
 
   const { data } = await apolloClient.query<GetCart>({
     query: GET_CART
@@ -20,6 +23,7 @@ export async function getServerSideProps() {
 
   return {
     props: {
+      session,
       items: cartMock,
       total: 'R$ 330,00',
       cards: cardsMock,
