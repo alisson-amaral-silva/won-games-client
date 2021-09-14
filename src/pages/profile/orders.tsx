d7c65e26b504345be90453274f6c9f7cd6e6d852
Alisson Amaral <aalissonamaral98@gmail.com>
2021-09-14 10:15:40 -0300

    ajustando a listagem de compras

--- src/pages/profile/orders.tsx
+++ src/pages/profile/orders.tsx
@@ -3,7 +3,11 @@ import ordersMock from 'components/OrdersList/mock'
 import { GetServerSidePropsContext } from 'next'
 import React from 'react'
 import Profile from 'templates/Profile'
+import { initializeApollo } from 'utils/apollo'
 import protectedRoutes from 'utils/protected-routes'
+import { GetOrders, GetOrdersVariables } from 'graphql/generated/GetOrders'
+import { GET_ORDERS } from 'graphql/queries/order'
+import { ordersMapper } from 'utils/mappers'
 
 export default function Cards({ items }: OrdersListProps) {
   return (
@@ -16,9 +20,19 @@ export default function Cards({ items }: OrdersListProps) {
 // Cada vez que o usuario add ou remove as coisas ela não é estatica por isso o serverSideProps
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   const session = await protectedRoutes(context)
+
+  const apolloClient = initializeApollo(null, session)
+
+  const { data } = await apolloClient.query<GetOrders, GetOrdersVariables>({
+    query: GET_ORDERS,
+    variables: {
+      identifier: session?.id
+    }
+  })
+
   return {
     props: {
-      items: ordersMock,
+      items: ordersMapper(data.orders),
       session
     }
   }
