1803e40d31fb9f29e5f49d80f790c51f5bb15156
Alisson Amaral <aalissonamaral98@gmail.com>
2021-09-22 11:08:15 -0300

    add e removendo do carrinho

--- cypress/e2e/game.ts
+++ cypress/e2e/game.ts
@@ -1,10 +1,12 @@
 /// <reference types="cypress" />
 
 describe('Game Page', () => {
-  it('should render game page sections', () => {
+  before(() => {
     //visitar a pagina de um game especifico
     cy.visit('/game/baldurs-gate-iii')
+  })
 
+  it('should render game page sections', () => {
     cy.wait(500)
 
     cy.getByDataCy('game-info').within(() => {
@@ -51,7 +53,36 @@ describe('Game Page', () => {
     })
 
 
-    cy.shouldRenderShowcase({name: 'Upcomming', highlight: true})
-    cy.shouldRenderShowcase({name: 'You may like these games', highlight: false})
+    cy.shouldRenderShowcase({ name: 'Upcomming', highlight: true })
+    cy.shouldRenderShowcase({ name: 'You may like these games', highlight: false })
+  })
+
+  it('should add/remove game into cart', () => {
+
+    cy.getByDataCy('game-info').within(() => {
+      cy.findByRole('button', { name: /add to cart/i }).click()
+      cy.findByRole('button', { name: /remove from cart/i }).should('exist')
+    })
+
+    cy.findAllByLabelText('Cart items')
+      .first()
+      .should('have.text', 1)
+      .click()
+
+    cy.getByDataCy('cart-list').within(() => {
+      cy.findByRole('heading', { name: /Baldur's Gate 3/i }).should('exist')
+      cy.findByText(/remover/i).click()
+
+      cy.findByRole('heading', { name: /Your cart is empty/i }).should('exist')
+    })
+
+    cy.findAllByLabelText('Shopping cart')
+      .first()
+      .click()
+
+
+    cy.getByDataCy('game-info').within(() => {
+      cy.findByRole('button', { name: /add to cart/i }).should('exist')
+    })
   })
 })
