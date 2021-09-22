828d9e0fefe77c665fc9aee5f728046269bc4af5
Alisson Amaral <aalissonamaral98@gmail.com>
2021-09-22 12:09:19 -0300

    teste de aparecer 15 jogos e solicitar mais jogos

--- cypress/e2e/explore.ts
+++ cypress/e2e/explore.ts
@@ -8,8 +8,11 @@ import {
 } from '../../src/utils/filter/fields'
 
 describe('Explore Page', () => {
-  it('should render filters columns' , () => {
+  before(() => {
     cy.visit('/games')
+  })
+
+  it('should render filters columns' , () => {
 
     cy.findByRole('heading', {name: /sort by price/i}).should('exist')
     cy.findByRole('heading', {name: /^price/i}).should('exist')
@@ -25,7 +28,11 @@ describe('Explore Page', () => {
   cy.getFields(categoriesFields)
   })
 
+  it('should show 15 games and show more games when show more is clicked', () => {
+    cy.getByDataCy('game-card').should('have.length',15)
 
+    cy.findByRole('button', {name: /show more/i}).click()
 
-
+    cy.getByDataCy('game-card').should('have.length',30)
+  })
 })
