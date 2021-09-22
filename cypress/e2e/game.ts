a0e9c38eb11d354ea068bf0d22ea6003086b37c2
Alisson Amaral <aalissonamaral98@gmail.com>
2021-09-22 09:52:39 -0300

    utilizando métodos children() para buscar filhos

--- cypress/e2e/game.ts
+++ cypress/e2e/game.ts
@@ -8,18 +8,27 @@ describe('Game Page', () => {
     cy.wait(500)
 
     cy.getByDataCy('game-info').within(() => {
-        cy.findByRole('heading', { name: /Baldur's Gate 3/i }).should('exist')
+      cy.findByRole('heading', { name: /Baldur's Gate 3/i }).should('exist')
 
-        cy.findByText(/^This game is currently in development/i).should('exist')
+      cy.findByText(/^This game is currently in development/i).should('exist')
 
-        cy.findByText("$199.99").should('exist')
+      cy.findByText("$199.99").should('exist')
 
-        cy.findByRole('button', { name: /add to cart/i}).should('exist')
+      cy.findByRole('button', { name: /add to cart/i }).should('exist')
 
-      })
+    })
 
-      cy.wait(500)
-      //gallery
-      cy.findAllByRole('button', {name: /^thumb\-/i}).should('have.length.gt', 0)
+    cy.wait(500)
+
+    //gallery
+    cy.findAllByRole('button', { name: /^thumb\-/i }).should('have.length.gt', 0)
+
+    //content
+    cy.getByDataCy('content').within(() => {
+      cy.findByRole('heading', { name: /description/i })
+    })
+
+    //o método children pega os filhos de cada elemento la dentro
+    cy.getByDataCy('content').children().should('have.length.at.least', 2)
   })
 })
